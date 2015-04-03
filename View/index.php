db87e3915395679b8cc58eb4273c9c725d3f88bd
Taiki Fnit <taikifnitspg@gmail.com>
2015-04-04 07:03:00 +0900

    imprement bootstrap and jquery ui

--- View/index.php
+++ View/index.php
@@ -8,32 +8,8 @@ $controller = new ExtractsController();
 // $index[ニュースの年号][ニュースの番号][ニュースの情報]
 $index = $controller->index();
 
-
-
 ?>
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-	<meta charset="utf-8">
-	<title>Control Panel</title>        
-	<meta name="viewport" content="width=device-width">
-</head>
-<body>
-	<h1>CMS</h1>
-	
-    <form action="http://localhost/control/Controller/PostsController.php" method="POST">
-    
-        <label for="formTitle">Title:</label>
-        <input type="text" name="title" id="formTitle">
-        
-        <label for="formContent">Content:</label>
-        <textarea name="content" id="formContent"></textarea>
-        
-        <input type="hidden" name="action" value="add">
-        
-        <input type="submit" value="Send!">
-    
-    </form>
+
 	
     <?php
         echo "<ul>";
@@ -46,6 +22,3 @@ $index = $controller->index();
         echo "</ul>";
     ?>
 	
-    
-</body>
-</html>
